935cdf45c8b95a3f06abc0666fb9e3ac7b28d038
November7 <m.kowalski.nov7@outlook.com>
2025-11-30 00:00:17 +0100

    Update version number to 1.0.3 and adjust build date in version.php

--- version.php
+++ version.php
@@ -1,8 +1,8 @@
 <?php
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2025112901;   // Data w formacie YYYYMMDDXX
+$plugin->version   = 2025113001;   // Data w formacie YYYYMMDDXX
 $plugin->requires  = 2022041900;   // Minimalna wersja Moodle
-$plugin->release   = "1.0.2 (Build: {$plugin->version})";
+$plugin->release   = "1.0.3 (Build: {$plugin->version})";
 $plugin->component = 'filter_githubcode';
 $plugin->name = 'GitHub Code Filter';
