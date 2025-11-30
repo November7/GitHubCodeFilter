4c113ab79144cd2bbce8b3afc66b2a42e2c49f56
November7 <m.kowalski.nov7@outlook.com>
2025-11-30 13:04:40 +0100

    Fix release version in version.php to 0.0.3

--- version.php
+++ version.php
@@ -3,6 +3,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->version   = 2025113002;   // Data w formacie YYYYMMDDXX
 $plugin->requires  = 2022041900;   // Minimalna wersja Moodle
-$plugin->release   = "1.0.3 (Build: {$plugin->version})";
+$plugin->release   = "0.0.3 (Build: {$plugin->version})";
 $plugin->component = 'filter_githubcode';
 $plugin->name = 'GitHub Code Filter';
