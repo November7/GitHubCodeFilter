067db6846c411923eea9a2eb355ec259ef14bd68
November7 <m.kowalski.nov7@outlook.com>
2025-11-30 12:51:10 +0100

    Update plugin version to 2025113002

--- version.php
+++ version.php
@@ -1,7 +1,7 @@
 <?php
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2025113001;   // Data w formacie YYYYMMDDXX
+$plugin->version   = 2025113002;   // Data w formacie YYYYMMDDXX
 $plugin->requires  = 2022041900;   // Minimalna wersja Moodle
 $plugin->release   = "1.0.3 (Build: {$plugin->version})";
 $plugin->component = 'filter_githubcode';
