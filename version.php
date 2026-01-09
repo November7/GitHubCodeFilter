9af1111d0f2f473438c38ed7f6e705d1ac58e992
Marcin Kowalski <m.kowalski.nov7@outlook.com>
2026-01-09 14:54:33 +0100

    Update version to 1.1.8 and increment build number

--- version.php
+++ version.php
@@ -1,8 +1,8 @@
 <?php
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2026010902;   // Data w formacie YYYYMMDDXX
+$plugin->version   = 2026010903;   // Data w formacie YYYYMMDDXX
 $plugin->requires  = 2022041900;   // Minimalna wersja Moodle
-$plugin->release   = "1.1.7 (Build: {$plugin->version})";
+$plugin->release   = "1.1.8 (Build: {$plugin->version})";
 $plugin->component = 'filter_githubcode';
 $plugin->name = 'GitHub Code Filter';
